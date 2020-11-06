e76f7ad7d7d8e09278ad23a7698442a5d618e459
DiegoSaragozaSilva <diegosaragozaa2@gmail.com>
2020-11-06 08:16:36 -0300

    SymbolTable finalizado

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -49,7 +49,7 @@ public class SymbolTable {
      */
     public Integer getAddress(String symbol) {
         /* TODO: implementar */
-    	return null;
+        return symbolTable.get(symbol);
     }
 
     /**
@@ -61,7 +61,17 @@ public class SymbolTable {
     //
     public void initialize() {
         // Exemplo: Registradores Virtuais
-        this.addEntry("R0", 0);
-        /* TODO: implementar */
+        for (int i = 0; i < 16; i++){
+            this.addEntry("R"+ i, i);
+        }
+
+        this.addEntry("SP", 0);
+        this.addEntry("LCL", 1);
+        this.addEntry("ARG", 2);
+        this.addEntry("THIS", 3);
+        this.addEntry("THAT", 4);
+        this.addEntry("SCREEN", 16384);
+        this.addEntry("LED", 21184);
+        this.addEntry("SW", 21185);
     }
 }
